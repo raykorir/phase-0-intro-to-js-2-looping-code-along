8228ac6eb29a3f77e29b3c556fe0f81859d00464
raymond <raymond.korir@student.moringaschool.com>
2024-04-04 12:08:04 +0300

    looping-code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
